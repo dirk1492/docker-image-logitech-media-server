40ed919e206a353c587fb1010c04454123642d6c
Dirk Lembke <dil001@web.de>
2017-08-26 19:28:48 +0200

    add start script and tag latest builÃd

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 sudo docker build -t dil001/logitechmediaserver-arm32v7:7.9.1 .
+sudo docker tag dil001/logitechmediaserver-arm32v7:7.9.1 dil001/logitechmediaserver-arm32v7:latest
---
+++ start.sh
@@ -0,0 +1,1 @@
+sudo docker run -d --name lms --restart always -p 9000:9000 -p 3483:3483 -p 3483:3483/udp -v /media/Data/squeezebox:/srv/squeezebox  -v /media:/media -v /etc/localtime:/etc/localtime:ro dil001/logitechmediaserver-arm32v7:7.9.1
