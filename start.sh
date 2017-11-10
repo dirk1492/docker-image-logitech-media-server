512b44168e0f077355ab9f4264c2139e87bb7ce0
Dirk Lembke <dil001@web.de>
2017-11-10 14:53:41 +0100

    expose port 9090

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-sudo docker run -d --name lms --restart always -p 9000:9000 -p 3483:3483 -p 3483:3483/udp -v /media/Data/squeezebox:/srv/squeezebox  -v /media:/media -v /etc/localtime:/etc/localtime:ro dil001/logitechmediaserver-arm32v7:7.9.1
+sudo docker run -d --name lms --restart always -p 9000:9000 -p 9090:9090 -p 3483:3483 -p 3483:3483/udp -v /media/Data/squeezebox:/srv/squeezebox  -v /media:/media -v /etc/localtime:/etc/localtime:ro dil001/logitechmediaserver-arm32v7:7.9.1
