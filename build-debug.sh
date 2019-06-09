9e611733dbf3d37c3740246a93c801793ec88e47
dil <dil001@web.de>
2019-06-09 23:10:00 +0200

    smal changes

---
+++ build-debug.sh
@@ -0,0 +1,2 @@
+sudo docker build --no-cache -t dil001/logitechmediaserver-arm32v7:7.9.1-debug -f Dockerfile.debug  .
+sudo docker tag dil001/logitechmediaserver-arm32v7:7.9.1-debug dil001/logitechmediaserver-arm32v7:debug
--- start.sh
+++ start.sh
@@ -7,6 +7,12 @@ fi
 if [ -f "$(pwd)/nginx.conf" ]; then
   if ! docker inspect lms-proxy >/dev/null 2>&1 ; then 
     echo "Start LMS proxy..."
-    docker run --name lms-proxy --restart always -p 80:80 -v $(pwd)/nginx.conf:/etc/nginx/conf.d/lms.conf:ro -d arm64v8/nginx
+    docker run --name lms-proxy --restart always -p 80:80 -v $(pwd)/nginx.conf:/etc/nginx/conf.d/lms.conf:ro -d arm64v8/nginx-alpine
   fi
+fi
+
+
+if ! docker inspect samba >/dev/null 2>&1 ; then
+	echo "Start samba..."
+	docker run -it --name samba --restart always -p 139:139 -p 445:445 -v /media:/media -d -e USERID=2000 dperson/samba:aarch64 -s "media;/media;yes;yes;yes;all;all;lms" -u "lms;km2412se" -w LMS -S -n -g "unix charset = utf-8" -g "dos charset = cp1252" 
 fi
