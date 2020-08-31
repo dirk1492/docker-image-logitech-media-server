7245f7558443f5d12c87f1ac2f4be16176c1fd16
Dirk Lembke <dirk@lembke.nz>
2020-09-01 10:47:34 +1200

    set correct tag

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-sudo docker build --no-cache -t dil001/logitechmediaserver-arm32v7:7.9.1 .
-sudo docker tag dil001/logitechmediaserver-arm32v7:7.9.1 dil001/logitechmediaserver-arm32v7:latest
+sudo docker build --no-cache -t dil001/logitechmediaserver-arm32v7:7.9.3 .
+sudo docker tag dil001/logitechmediaserver-arm32v7:7.9.3 dil001/logitechmediaserver-arm32v7:latest
